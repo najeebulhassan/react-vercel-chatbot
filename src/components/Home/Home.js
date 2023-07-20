29774c2907dfd31281796090f3d2f46cf85eac28
najeebulhassan <umuhammadnajeeb@gmail.com>
2023-07-21 02:08:43 +0500

    update notes button with tooltip

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,6 +3,8 @@ import ChatBox from './ChatBox/ChatBox';
 import ColorScheme from './ColorScheme/ColorScheme';
 import IntroArea from './IntroArea/IntroArea';
 import Header from './Header/Header';
+import Tooltip from '@mui/material/Tooltip';
+import SpeakerNotesIcon from '@mui/icons-material/SpeakerNotes';
 
 export default function Home() {
     const [checked, setChecked] = useState(false);
@@ -12,6 +14,15 @@ export default function Home() {
 
     return (
         <div className="font-sans antialiased __variable_0ec1f4 __variable_71fa92">
+            <Tooltip title="Notes" placement="right-start">
+                <SpeakerNotesIcon style={{
+                    position: 'absolute', // Position the icon absolutely
+                    top: '150px', // Adjust the top position as needed
+                    left: '20px', // Adjust the left position as needed
+                    width: '30px', // Adjust the width of the icon as needed
+                    height: '30px', // Adjust the height of the icon as needed
+                }} />
+            </Tooltip>
             <div
                 style={{
                     position: 'fixed',
